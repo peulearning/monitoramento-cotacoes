3f0bab87063b59cf958b21f3cb5861f9af7f56f7
Pedro <peuhenry2018@gmail.com>
2025-11-28 14:22:47 -0300

    Caso de uso Processar Cotacao API

--- src/main/java/com/portfolio/monitoramento_cotacoes/domain/port/in/ProcessarCotacaoUseCase.java
+++ src/main/java/com/portfolio/monitoramento_cotacoes/domain/port/in/ProcessarCotacaoUseCase.java
@@ -1,12 +1,9 @@
 package com.portfolio.monitoramento_cotacoes.domain.port.in;
 
-import java.util.Optional;
-
-import com.portfolio.monitoramento_cotacoes.domain.entity.Cotacao;
 
 public interface ProcessarCotacaoUseCase {
 
   //Executar a busca na API externa e salvar o resultado no BD
-  Optional<Cotacao> processarCotacao(String moedaOrigem, String moedaDestino);
+  void processarCotacao(String moedaOrigem, String moedaDestino);
 
 }
